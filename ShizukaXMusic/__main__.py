9e384bb933a718fd6a9ae510ecba3f00f3870fb8
DIGITAL BOT XD <128338349+DIGITALBOTXD@users.noreply.github.com>
2023-04-17 08:45:43 +0530

    Update __main__.py

--- ShizukaXMusic/__main__.py
+++ ShizukaXMusic/__main__.py
@@ -7,10 +7,10 @@ from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
 from config import BANNED_USERS
-from ShizukaXMusic import LOGGER, app, userbot
-from ShizukaXMusic.core.call import Shizuka
-from ShizukaXMusic.plugins import ALL_MODULES
-from ShizukaXMusic.utils.database import get_banned_users, get_gbanned
+from XDMUSIC import LOGGER, app, userbot
+from XDMUSIC.core.call import Shizuka
+from XDMUSIC.plugins import ALL_MODULES
+from XDMUSIC.utils.database import get_banned_users, get_gbanned
 
 loop = asyncio.get_event_loop()
 
@@ -23,7 +23,7 @@ async def init():
         and not config.STRING4
         and not config.STRING5
     ):
-        LOGGER("ShizukaXMusic").error("Add Pyrogram string session and then try...")
+        LOGGER("XDMUSIC").error("Add Pyrogram string session and then try...")
     try:
         users = await get_gbanned()
         for user_id in users:
@@ -35,24 +35,24 @@ async def init():
         pass
     await app.start()
     for all_module in ALL_MODULES:
-        importlib.import_module("ShizukaXMusic.plugins" + all_module)
+        importlib.import_module("XDMUSIC.plugins" + all_module)
     LOGGER("ShizukaXMusic.plugins").info("Necessary Modules Imported Successfully.")
     await userbot.start()
     await Shizuka.start()
     try:
         await Shizuka.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
-        LOGGER("ShizukaXMusic").error(
+        LOGGER("XDMUSIC").error(
             "[ERROR] - \n\nTurn on group voice chat and don't put it off otherwise I'll stop working thanks."
         )
         sys.exit()
     except:
         pass
     await Shizuka.decorators()
-    LOGGER("ShizukaXMusic").info("Music Bot Started Successfully")
+    LOGGER("XDMUSIC").info("Music Bot Started Successfully")
     await idle()
 
 
 if __name__ == "__main__":
     loop.run_until_complete(init())
-    LOGGER("ShizukaXMusic").info("Stopping Music Bot")
+    LOGGER("XDMUSIC").info("Stopping Music Bot")
